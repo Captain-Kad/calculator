2bdd8723aa3d9ce33c74000760e86354a50e9e5c
Bukunmi Kadri <70661092+Captain-Kad@users.noreply.github.com>
2020-09-23 20:57:45 -0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function appendAndSolve(btnId) {
+  var buttonVal = document.getElementById(btnId).value;
+  document.getElementById("result").innerHTML += buttonVal;
+
+  if (btnId === "clear") {
+    document.getElementById("result").innerHTML = null;
+  }
+
+  if (btnId === "answer") {
+    res = document.getElementById("result").innerHTML;
+    document.getElementById("result").innerHTML = eval(res);
+  }
+}
+
+function changeSymbol(btnId) {
+  if (btnId === "answer") {
+    res = document.getElementById("result").innerHTML;
+    res = res.replace(/÷/g, "/");
+    res = res.replace(/×/g, "*");
+    if (res.charAt(0) === "√") {
+      res = Math.sqrt(res.slice(1));
+    }
+    document.getElementById("result").innerHTML = eval(res);
+  }
+}
